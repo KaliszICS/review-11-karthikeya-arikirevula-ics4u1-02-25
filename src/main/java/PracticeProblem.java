98df4dc6c04c51851d146a0873b3d315453204c2
karthikeya-arikirevula-ics4u1-02-25 <karthikeya.arikirevula@student.tdsb.on.ca>
2025-02-28 14:52:01 +0000

    done

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,5 +4,42 @@ public class PracticeProblem {
 
 	}
 
+	public static int[] createIntArray() {
+		int[] arr = new int[5];
 
+		for (int i = 0; i < 5; i++) {
+			arr[i] = i + 1;
+		}
+		return arr;
+	}
+
+	public static String[] createArray(String one, String two, String three, String four) {
+		String[] stringArray = {one, two, three, four};
+		return stringArray;
+	}
+
+	public static int findValue(int num, int[] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] == num) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	public static int findThirdValue(String word, String[] arr) {
+		int count = 0;
+
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i].equals(word)) {
+				count++;
+				if (count == 3) {
+					return i;
+				}
+			}
+		}
+		return -1;
+	}
 }
+
+
